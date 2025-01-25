b413af63dd11c05f8ca02e8c238d4fbf6514b909
r2ditto <ryan.ebro@yahoo.com>
2025-01-24 21:35:05 -0500

    set up tailwind and geist font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Geist } from "next/font/google";
+
+import "@/styles/globals.css";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${geist.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
